1401afaa87c0ed9c8197f6792a859340e81b7c17
OlehKSS <olehkss@github.com>
2018-05-06 15:43:34 +0200

    ee initial position set on vrep

--- main.m
+++ main.m
@@ -12,7 +12,7 @@ import kinematics.get_quaternion;
 import kinematics.DirectKinematics;
 
 % inverse or transpose (of Jacobian)
-algorithm = 'inverse';
+algorithm = 'transpose';
 % initial position
 x_pos = [0.65 0 0.4]';
 % desired quaternion
@@ -23,8 +23,8 @@ x_q = [cos(0.5*theta); sin(0.5*theta)*x_dir];
 
 r_i = get_init_rot_matrix(x_dir);
 r_f = get_rot_matrix(x_dir, theta);
-x_q_test = get_quaternion(r_f);
-q = get_initial_configuration(x_pos, x_q, algorithm);
+x_q_i = get_quaternion(r_i);
+q = get_initial_configuration(x_pos, x_q_i, algorithm);
 
 % how to find cruise velocity
 % NOTE: A.*B multiplies A and B element by element. It is different from 
--- main_vrep.m
+++ main_vrep.m
@@ -23,101 +23,32 @@ function [t, q, q_act] = main_vrep
     import utils.get_initial_configuration;
 
     porta = 19997;          % default V-REP port
-    tf = 4;                 % final time
-    Ts = 0.01;              % sampling time
-    t  = 0:Ts:tf;           % time vector
-    N  = length(t);         % number of points of the simulation 
-    n = 7;                  % joint number
-    q  = zeros(n,N);        % q(:,i) collects the joint position for t(i)
-    dq  = zeros(n,N);       % q(:,i) collects the joint position for t(i)
-    % we can set initial configuration of the joints, but not the initial
-    % position of the end-effector
-    q(:,1) = [0 35  20 -45 30 30 -20]'/180*pi;   % initial configuration
     
     % << Initialization additional variables that I would like to use
-    % inverse or transpose (of Jacobian)
-    algorithm = 'inverse';
-    % desired position
-    x_pos = [0.65 0 0.4]';
-    pos_error_t = zeros(3, N);
-    % desired quaternion
-    x_dir = [1 -1 0]';
-    x_dir = x_dir / norm(x_dir);
-    theta = pi/2;
-    x_q = [cos(0.5*theta); sin(0.5*theta)*x_dir];
-
-    % q_initial = get_initial_configuration(x_pos, x_q, algorithm);
-    % disp(q_initial);
    q_initial = [-0.1201
-    -0.7300
+    0.7300
     0.3238
    -1.4300
     1.0247
-    1.1270
+   -1.1270
     0.6155];
 
-%     if strcmp(algorithm, 'inverse')
-%         K = diag(10*ones(1, 6));
-%         % the larger K the larger robot movement
-%         % settling time will be 10*constant time (inverse of K diagonal values)
-%         dq_f = @(e, J_a) (pinv(J_a)*(K*e));
-%     else
-%         K = diag(20*ones(1, 6));
-%         dq_f = @(e, J_a) (J_a'*(K*e));
-%     end
-
     % >> End of my initialization block
-    clc
     fprintf('----------------------');
     fprintf('\n simulation started ');
     fprintf('\n trying to connect...\n');
     [clientID, vrep ] = StartVrep(porta);
     
+    % start simulation of vrep
+    [returnCode]= vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot);
+    pause(2);
+    
     handle_joint = my_get_handle_Joint(vrep,clientID);      % handle to the joints
     %my_set_joint_target_position(vrep, clientID, handle_joint, q(:,1)); % first move to q0
     % set initial pose of the manipulator
     my_set_joint_target_position(vrep, clientID, handle_joint, q_initial);
-    q_act(:,1) = my_get_joint_target_position(clientID,vrep,handle_joint,n);% get the actual joints angles from v-rep
-    
-    % main simulation loop
-%     for i=2:N
-%         % << My code for calculating trajectory of the arm
-%         kuka_joint_temp = q(:, i-1);
-%         T = kuka_directkinematics(kuka_joint_temp);
-%         J = kuka_J(kuka_joint_temp);
-%         % In case of quaternion representation geometric Jacobian and
-%         % analytical are the same
-%         
-%         x_pos_current = T(1:3, 4);
-%         x_q_current = get_quaternion(T(1:3, 1:3));
-%         % calculate separately error for the quaternion and pos vector
-%         error_pos = x_pos - x_pos_current;
-%         pos_error_t(:, i) = error_pos;
-%         error_quat = get_orientation_error(x_q, x_q_current);
-%         quat_error_t(:, i) = error_quat;
-%         error = [error_pos; error_quat];
-%         dq(:, i) = dq_f(error, J);
-%         % >> End of my code
-%         
-%         q(:,i) = q(:,i-1) + Ts*dq(:,i);
-%         my_set_joint_target_position(vrep, clientID, handle_joint, q(:,i));
-%         q_act(:,i) = my_get_joint_target_position(clientID,vrep,handle_joint,n);% get the actual joints angles from v-rep     
-%     end
-    
-    % << My code for checking of the errors
-    % Errors converge go to zero in the end of the simulation
-%     figure;
-%     plot(t, pos_error_t);
-%     title('Position error');
-%     
-%     figure;
-%     plot(t, quat_error_t);
-%     title('Quaternion error');
-%     
-    % << End of my code
-
-    DeleteVrep(clientID, vrep); 
-            
+    pause(2);
+    DeleteVrep(clientID, vrep);      
 end
 
 % constructor
