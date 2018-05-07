721f15468f988c10c42bffc1617930c3e79820c2
OlehKSS <olehkss@github.com>
2018-05-08 00:19:15 +0200

    fixing of the quaternion ambiguity

--- +kinematics/get_orientation_error.m
+++ +kinematics/get_orientation_error.m
@@ -1,6 +1,26 @@
 function error = get_orientation_error(qe, qd)
 % qe - first quaternion, actual
 % qd - second quaternion, desired
+% 
+% if (qe(1) < 0) % I need to check only this
+%     % since two quaternions [a, b] = [-a, -b] represent 
+%     % I will need to add this type of check for every time I do
+%     % quaternion calculation
+%     % the same rotation
+%     qe(1) = -qe(1);
+%     qe(2:end) = -qe(2:end);    
+% end
+% 
+% if (qd(1) < 0)
+%     qd(1) = -qd(1);
+%     qd(2:end) = -qd(2:end);
+% end
 
 error = qe(1)*qd(2:end) - qd(1)*qe(2:end) - cross(qd(2:end),qe(2:end));
+
+scalar_error = qe(1)*qd(1) + (qd(2:end)')*qe(2:end);
+
+    if (scalar_error < 0)
+        error = -error;
+    end
 end
--- +kinematics/get_quaternion.m
+++ +kinematics/get_quaternion.m
@@ -1,14 +1,22 @@
 function q = get_quaternion(R)
-% R - rotation matrix to be transformed
-etha = 0.5 * sqrt(R(1,1) + R(2,2) + R(3,3) +1);
+    % Returns quaternion for the given rotation matrix
+    % R - rotation matrix to be transformed
+    etha = 0.5 * sqrt(R(1,1) + R(2,2) + R(3,3) +1);
 
-epsilon = [
-    sign(R(3,2) - R(2,3))*0.5*sqrt(R(1,1) - R(2,2) - R(3,3) + 1);
-    sign(R(1,3) - R(3,1))*0.5*sqrt(R(2,2) - R(3,3) - R(1,1) + 1);
-    sign(R(2,1) - R(1,2))*0.5*sqrt(R(3,3) - R(1,1) - R(2,2) + 1);
-    ];
+    epsilon = [
+        sign(R(3,2) - R(2,3))*0.5*sqrt(R(1,1) - R(2,2) - R(3,3) + 1);
+        sign(R(1,3) - R(3,1))*0.5*sqrt(R(2,2) - R(3,3) - R(1,1) + 1);
+        sign(R(2,1) - R(1,2))*0.5*sqrt(R(3,3) - R(1,1) - R(2,2) + 1);
+        ];
 
-q = [etha; epsilon];
-q = real(q);
-q = q/norm(q);
+    % since two quaternions [a, b] = [-a, -b] represent the same rotation, we
+    % need to stick to one representation only, we need to check only scalar
+    % part of the quaternion
+    if (etha < 0)
+        epsilon = -epsilon;
+    end
+
+    q = [etha; epsilon];
+    q = real(q);
+    q = q/norm(q);
 end
